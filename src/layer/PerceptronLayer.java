35fe96480823f856ad4ee9ac8437f49e394f14d6
Meowskr <Johnathondav@gmail.com>
2026-01-16 21:56:53 -0600

    Backpropogation effort

--- src/layer/PerceptronLayer.java
+++ src/layer/PerceptronLayer.java
@@ -8,6 +8,8 @@ public class PerceptronLayer {
     private double[][] weights;
     private double[] biases;
     private int neuronCount;
+    private double[] lastWeightedSum;
+    private double[] lastInput;
 
     public PerceptronLayer(int neurons, int inputCount, long SEED){
         this.weights = new double[neurons][inputCount];
@@ -22,8 +24,35 @@ public class PerceptronLayer {
     }
 
 
-    public double[] getOutput(double[] input){
-        return TensorUtil.reLU(TensorUtil.add(TensorUtil.multiply(weights, input), biases));
+    public double[] forwardPass(double[] input){
+        this.lastInput = input;
+        this.lastWeightedSum = TensorUtil.add(TensorUtil.multiply(weights, input), biases);
+        return TensorUtil.reLU(this.lastWeightedSum);
     }
 
+    public double[] backwardPass(double[] dLdA){
+        double[] dLdX = new double[weights[0].length];
+        double dLdW;
+        double dAdZ;
+        double dZdW;
+        double dZdX;
+        for(int i = 0; i < weights[0].length; i++){
+            double dLdX_Sum = 0;
+            for(int j = 0; j< weights.length; j++){
+                dAdZ = derivativeRelu(lastWeightedSum[j]);
+                dZdW = lastInput[j];
+                dLdW = dLdA[j]*dAdZ*dZdW;
+                dZdX = weights[i][j];
+                weights[i][j] -= 0.01*dLdW;
+                dLdX_Sum += dLdA[j]*dAdZ*dZdX;
+            }
+            dLdX[i] = dLdX_Sum;
+        }
+        return dLdX;
+    }
+
+    public static double derivativeRelu(double scalar){
+        if(scalar <= 0) return 1;
+        else return 0.01;
+    }
 }
--- src/tensor/TensorUtil.java
+++ src/tensor/TensorUtil.java
@@ -5,9 +5,9 @@ public class TensorUtil {
     public static double[] multiply(double[][] matrix, double[] vector){
         int cols = matrix[0].length;
         int rows = matrix.length;
-        double[] output = new double[cols];
+        double[] output = new double[rows];
         for (double[] doubles : matrix) {
-            for (int j = 0; j < cols; j++) {
+            for (int j = 0; j < rows; j++) {
                 output[j] = vector[j] * doubles[j];
             }
         }
@@ -47,6 +47,30 @@ public class TensorUtil {
         double[] output = new double[len];
         for(int i = 0; i < len; i++)
             output[i] = Math.max(0, vector[i]);
+        return output;
+    }
+
+    public static double loss(double[] actual, double[] predicted){
+        double output = 0;
+        for(int i = 0; i < predicted.length; i++){
+            output += 0.5*Math.pow(actual[i] - predicted[i], 2.0);
+        }
+        return output;
+    }
+
+    public static double[] derivativeLoss(double[] actual, double[] predicted){
+        double[] output = new double[actual.length];
+        for(int i = 0; i < predicted.length; i++){
+            output[i] = actual[i] - predicted[i];
+        }
+        return output;
+    }
+
+    public static double[] sigmoid(double[] vector){
+        double[] output = new double[vector.length];
+        for(int i = 0; i < vector.length; i++){
+            output[i] = 1.0/(1+Math.exp(vector[i]*-1.0));
+        }
         return output;
     }
 
